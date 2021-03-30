f6b90bc56b12993f614cba7f3ec163b47b5e2b95
Fickos <filip.kresa@gmail.com>
2021-03-30 15:14:29 +0200

    Redesigned page for displaying patients.

--- frontend/public/app/patientsList.js
+++ frontend/public/app/patientsList.js
@@ -11,9 +11,17 @@ Vue.component('patients-list', {
     */
 	template:
 	`<div>
-		<input type="text" v-model="searchName"/>
-		<button type="button" v-on:click="searchPatients()">Search</button>
 		<h1>List of patients</h1>
+
+		<div class="input-group">
+			<div class="form-outline">
+				<input type="search" id="form1" class="form-control" v-model="searchName"/>
+			</div>
+			<button type="button" class="btn btn-primary" v-on:click="searchPatients()">
+				<i class="fas fa-search"></i>
+			</button>
+		</div>
+
 		<table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
   			<thead>
 			  <tr>
@@ -45,10 +53,19 @@ Vue.component('patients-list', {
 	},
 	methods: {
 		searchPatients: function(){
-			alert(`Sending request with param , ${this.searchName}`)
-			axios
+			if(this.searchName == "")
+			{
+				axios
+				.get('http://localhost:8080/patients/all')
+				.then(response => {this.patients = response.data; console.log(this.patients)})
+			}
+			else
+			{
+				axios
 				.get(`http://localhost:8080/patients/${this.searchName}`)
 				.then(response => {this.patients = response.data; console.log(this.patients);})
+			}
+			
 		}
 	}
   })
