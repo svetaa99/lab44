64a8fa2b97da4bafbf656f7555c99a18d6e6ff85
Fickos <filip.kresa@gmail.com>
2021-03-30 12:23:22 +0200

    Database initialized. Table for patients created and filled with test data.

--- backend/src/main/java/backend/models/Patient.java
+++ backend/src/main/java/backend/models/Patient.java
@@ -1,13 +1,28 @@
 package backend.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Patient {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false)
 	private String name;
 	
+	@Column(name = "surname", nullable = false)
 	private String surname;
-	
+
+	@Column(name = "address", nullable = false)
 	private String address;
-	
+
+	@Column(name = "category", nullable = true)
 	private String category;
 	
 	public Patient() {
@@ -52,6 +67,35 @@ public class Patient {
 
 	public void setCategory(String category) {
 		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Patient))
+			return false;
+		Patient other = (Patient) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", surname=" + surname + ", address=" + address + ", category="
+				+ category + "]";
 	}
 	
 	
---
+++ backend/src/main/resources/data-postgres.sql
@@ -0,0 +1,5 @@
+insert into patient (name, surname, address, category) values ('Filip', 'Volaric', 'Nikole Tesle 71/A/6', 'GOLD');
+insert into patient (name, surname, address, category) values ('Svetozar', 'Vulin', 'JNA 50', 'GOLD');
+insert into patient (name, surname, address, category) values ('Uros', 'Petric', 'Zabaljska 1', 'PLATINUM');
+insert into patient (name, surname, address, category) values ('Cika', 'Ljave', '123', 'BRONZE');
+insert into patient (name, surname, address, category) values ('Pera', 'Peric', 'Pere Perića 20', 'SILVER');
