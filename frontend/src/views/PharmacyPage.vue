37371a77b16326f1e2e98f4c1aa6a56061025e9b
svetaa99 <svetozar.vulin@gmail.com>
2021-04-08 19:10:49 +0200

    Refactored PharmacyPage for medicine search.

--- frontend/src/views/PharmacyPage.vue
+++ frontend/src/views/PharmacyPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>{{pharmacy.name}}</h1>
+    <MedicinesSearch v-model="searchName" @clicked="onSearchClick"/>
     <MedicinesList :medicines="this.medicines"/>
   </div>
 </template>
@@ -9,6 +10,7 @@
 import axios from 'axios'
 import { config } from '@/config.js'
 import MedicinesList from '@/components/MedicinesList'
+import MedicinesSearch from '@/components/MedicinesSearch'
 
 const API_URL = config.API_URL
 
@@ -16,11 +18,13 @@ export default {
   name: 'PharmacyPage',
   components: {
     MedicinesList,
+    MedicinesSearch
   },
   data() {
     return {
       pharmacy: {},
       medicines: [],
+      searchName: '',
     }
   },
   mounted() {
@@ -32,7 +36,39 @@ export default {
 
     axios
       .get(`${API_URL}/pharmacy-medicines/get-medicines/${id}`)
-      .then(response => {this.medicines = response.data; console.log(response.data)})
+      .then(response => {
+        const pms = response.data
+        pms.map(pm => {
+          this.medicines.push(pm.medicine)
+        })
+      })
+  },
+  methods: {
+    onSearchClick(searchName) {
+      if (searchName == '') {
+        axios
+          .get(`${API_URL}/pharmacy-medicines/get-medicines/${this.pharmacy.id}`)
+          .then(response => {
+            const pms = response.data
+            this.medicines = []
+            pms.map(pm => {
+              this.medicines.push(pm.medicine)
+            })
+      })
+      } 
+      else {
+        axios
+          .get(`${API_URL}/pharmacy-medicines/get-medicines-by-name/${searchName}`)
+          .then(response => {
+            const pms = response.data
+            this.medicines = []
+            pms.map(pm => {
+              this.medicines.push(pm.medicine)
+            })
+            console.log(this.medicines)
+          })
+      }
+    }
   }
 }
 </script>
