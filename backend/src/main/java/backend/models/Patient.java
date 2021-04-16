a817b7d8cae0b374ffe551650c8a32dea94e6563
svetaa99 <svetozar.vulin@gmail.com>
2021-04-16 15:03:41 +0200

    Implemented Reservation entity for table.

--- backend/src/main/java/backend/models/Patient.java
+++ backend/src/main/java/backend/models/Patient.java
@@ -1,10 +1,13 @@
 package backend.models;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class Patient extends User{
 	
 	@Column(name = "category", nullable = true)
 	private String category;
+	
+	@OneToMany(mappedBy = "patient")
+	private List<Reservation> reservations;
 	
 	public Patient() {
 		
---
+++ backend/src/main/java/backend/models/Reservation.java
@@ -0,0 +1,88 @@
+package backend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="reservations")
+public class Reservation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "patient_id")
+	Patient patient;
+	
+	@ManyToOne
+	@JoinColumn(name = "medicine_id")
+	Medicine medicine;
+	
+	@Column(name = "quantity")
+	int quantity;
+	
+	@Column(name = "total_price")
+	double total_price;
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Long id, Patient patient, Medicine medicine, int quantity, double total_price) {
+		super();
+		this.id = id;
+		this.patient = patient;
+		this.medicine = medicine;
+		this.quantity = quantity;
+		this.total_price = total_price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Medicine getMedicine() {
+		return medicine;
+	}
+
+	public void setMedicine(Medicine medicine) {
+		this.medicine = medicine;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getTotal_price() {
+		return total_price;
+	}
+
+	public void setTotal_price(double total_price) {
+		this.total_price = total_price;
+	}
+	
+	
+}
