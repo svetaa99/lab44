512cd590cee09437980ba5a9ffac3a788c93f598
Uroš Petrić <urkepetric1999@gmail.com>
2021-04-25 20:17:06 +0200

    Small changes in Pharmacy backend

--- frontend/src/views/PharmacyPharmacistPage.vue
+++ frontend/src/views/PharmacyPharmacistPage.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-      <div class="container">
-
-      </div>
-      <br>
-      <!-- lista farmaceuta -->
+    <div class="container"></div>
+    <pharmacists-list :pharmacists="this.pharmacists"></pharmacists-list>
+    <br />
+    <!-- lista farmaceuta -->
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
+
 import { config } from "@/config.js";
 import PharmacistsList from "@/components/PharmacistsList.vue";
 const API_URL = config.API_URL;
 export default {
-    components: {
-        "pharmacists-list": PharmacistsList
-    },
-    data() {
-        return {
-            pharmacists: [],
-        };
-    },
-    mounted() {
-        const arr = window.location.href.split("/");
-        const id = arr[arr.length - 1];
-        axios.get(`${API_URL}/pharmacists/${id}`).then((response) => {
-            this.pharmacists = response.data;
-        });
-    }
-}
+  components: {
+    "pharmacists-list": PharmacistsList,
+  },
+  data() {
+    return {
+      pharmacists: [],
+    };
+  },
+  mounted() {
+    const arr = window.location.href.split("/");
+    const id = arr[arr.length - 2];
+    axios.get(`${API_URL}/pharmacist/${id}`).then((response) => {
+      this.pharmacists = response.data;
+      console.log(this.pharmacists)
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
