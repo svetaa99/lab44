dacbd57777b5630caae095a3ff4ad4562b03e5e7
Fickos <filip.kresa@gmail.com>
2021-06-15 22:34:28 +0200

    PDFs

--- backend/src/main/resources/data-postgres.sql
+++ backend/src/main/resources/data-postgres.sql
@@ -163,7 +163,7 @@ insert into vacation (finish, start, status, type, doctor_id) values ('2021-12-3
 
 insert into visit (patient_id, doctor_id, start_time, finish_time, pharmacy_id, status) values (11, 1, '2021-05-19 13:00:00', '2021-05-19 17:00:00', 3, 0);
 insert into visit (patient_id, doctor_id, start_time, finish_time, pharmacy_id, status) values (12, 1, '2021-05-19 13:30:00', '2021-05-19 19:00:00', 3, 0);
-insert into visit (patient_id, doctor_id, start_time, finish_time, pharmacy_id, status) values (10, 1, '2021-05-26 8:30:00', '2021-05-26 10:30:00', 1, 0);
+insert into visit (patient_id, doctor_id, start_time, finish_time, pharmacy_id, status) values (10, 1, '2021-06-15 22:00:00', '2021-06-15 23:30:00', 1, 0);
 
 insert into ratings (mark, obj_id, patient_id, type) values (5, 1, 10, 1);
 insert into ratings (mark, obj_id, patient_id, type) values (5, 1, 11, 1);
